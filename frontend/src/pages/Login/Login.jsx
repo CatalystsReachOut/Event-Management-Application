fa0ab1d0759f81e188c9ce5b4a27ad4aff6713a7
sitanshu05 <01fe20bcs260@kletech.ac.in>
2022-10-25 23:43:59 +0530

    Login and SignUp page completed

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,31 +1,26 @@
 import React from 'react'
-import Logo from '../../components/Logo/Logo'
 import "./Login.css"
 import email_icon from "../../assets/icons/email.png"
 import pass_icon from "../../assets/icons/password.png"
+import TextNIcon from '../../components/TextNIcon/TextNIcon'
+import { ROUTES } from '../../routes/RouterConfig'
 
 const styles = {
     body: "login__body flex items-center justify-center h-screen bg-hero bg-no-repeat bg-cover bg-right-top bg-fixed font-openSans",
-    login__container: "login__container flex flex-col items-center justify-around py-16 border-0 border-black px-4 h-3/4 bg-white rounded-2xl shadow-login_shadow m-2",
-    login__title: "login__title text-left w-96", 
+    login__container: "login__container flex flex-col items-center justify-around py-[60px] border-0 border-black px-[30px] bg-white rounded-[8px] shadow-login_shadow m-2 w-[35%] max-w-[420px]",
+    login__title: "login__title text-left w-full",
     login__title_h1: "login__title--h1 text-5xl pb-0 font-bold",
-    login__form: "login__form",
-    email_div: "emila_div flex justify-around ",
-    email_label: "email_label",
-    email_icon: 'mr-2',
-    email: "login__username border-b-2 border-font_orange block py-1 mb-5 px-1 w-full pb-3 focus:outline-none",
-    password_div: "password_div flex justify-around mb-2",
-    password_icon: "mr-2",
-    password: "login__password border-b-2 border-font_orange block py-1 mb-5 px-1 w-full pb-3 focus:outline-none",
-    checkbox__container: "inline ",
+    login__form: "login__form w-full pt-[60px]",
+    RemFor_container: "flex justify-between items-center mt-[30px]",
+    checkbox__container: "checkbox__container flex items-center",
     checkbox__box: "accent-font_orange",
-    checkbox__label: "ml-1",
-    forgotPass__conatiner: "inline right-0 ml-28 ",
-    forgotPass__link : "forgotPass__link hover:underline hover:decoration-solid ",
-    login__button: "login__button border-black border-2 text-center w-96 text-2xl font-semibold block mt-6 rounded-2xl border-font_orange p-0.5 text-center bg-font_orange text-white transition-colors hover:bg-font_orange-hover hover:ease-in",
-    signUp__container : "flex",
-    signUp__text : "",
-    signup__link: "login__signup pl-2 font-semibold hover:underline hover:decoration-solid" 
+    checkbox__label: "ml-1 checkbox__label",
+    forgotPass__conatiner: "ml-auto",
+    forgotPass__link: "forgotPass__link hover:underline hover:decoration-solid ",
+    login__button: "login__button text-center w-full text-2xl font-semibold block mt-[30px] rounded-[8px] border-font_orange py-[10px] text-center bg-font_orange text-white transition-colors hover:bg-font_orange-hover hover:ease-in",
+    signUp__container: "flex mt-[40px]",
+    signUp__text: "",
+    signup__link: "login__signup pl-2 font-semibold hover:underline hover:decoration-solid"
 }
 
 function Login() {
@@ -36,30 +31,31 @@ function Login() {
                     <h1 className={styles.login__title_h1}>Log In</h1>
                 </div>
                 <form action="" className={styles.login__form}>
-                    <div className={styles.email_div}>
-                        <label htmlFor="email" className={styles.email_label}>
-                            <img src={email_icon} alt="email_icon" className={styles.email_icon} />
-                        </label>
-                        <input type="email" className={styles.email} id="email" placeholder='Email address' />
+
+                    <TextNIcon
+                        icon={email_icon}
+                        type="email"
+                        placeholder="Email address" />
+                    <TextNIcon
+                        icon={pass_icon}
+                        type="password"
+                        placeholder="Password" />
+
+                    <div className={styles.RemFor_container}>
+                        <div className={styles.checkbox__container}>
+                            <input type="checkbox" id='rememberMe' className={styles.checkbox__box} />
+                            <label htmlFor="rememberMe" className={styles.checkbox__label}>Remember Me</label>
+                        </div>
+                        <div className={styles.forgotPass__conatiner}>
+                            <a href="" className={styles.forgotPass__link}>Forgot Password?</a>
+                        </div>
                     </div>
-                    <div className={styles.password_div}>
-                        <label htmlFor="email" className={styles.password_label}>
-                            <img src={pass_icon} alt="password_icon" className={styles.password_icon} />
-                        </label>
-                        <input type="password" className={styles.password} id="password" placeholder='Password' />
-                    </div>
-                    <div className={styles.checkbox__container}>
-                        <input type="checkbox" id='rememberMe' className={styles.checkbox__box} />
-                        <label htmlFor="rememberMe" className={styles.checkbox__label}>Remember Me</label>
-                    </div>
-                    <div className={styles.forgotPass__conatiner}>
-                        <a href="" className={styles.forgotPass__link}>Forgot Password?</a>
-                    </div>
+
                     <button type="submit" className={styles.login__button}>Log In</button>
                 </form>
                 <div className={styles.signUp__container}>
                     <p className={styles.signUp__text}> Need an account?</p>
-                    <a href="" className={styles.signup__link}>Sign Up</a>
+                    <a href={ROUTES.SignUp} className={styles.signup__link}>Sign Up</a>
                 </div>
             </div>
         </div>
--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -3,8 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import About from '../pages/About/About';
 import Home from '../pages/Home/Home';
 import { ROUTES } from './RouterConfig';
-import Test from "../pages/Test/Test"
 import Login from "../pages/Login/Login"
+import SignUp from '../pages/SignUp/SignUp';
 
 const Router = () => {
 
@@ -21,7 +21,8 @@ const Router = () => {
         <Routes>
             <Route exact path={ROUTES.Home} element={<RouteWithRole Element={Home} />}></Route>
             <Route exact path={ROUTES.About} element={<RouteWithRole Element={About} />}></Route>
-            <Route exact path={"/Login"} element={<RouteWithRole Element={Login} />}></Route>
+            <Route exact path={ROUTES.Login} element={<RouteWithRole Element={Login} />}></Route>
+            <Route exact path={ROUTES.SignUp} element={<RouteWithRole Element={SignUp} />}></Route>
         </Routes>
     </div>
   )
